4c0dabd98ce83998ebc1e291467d9e7df355ccbd
Sylvain UTARD <sylvain.utard@gmail.com>
2014-03-30 16:36:43 -0700

    Be social!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LinkedinSearch::Application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
+  get "/auth/failure", to: redirect('/')
   delete "/signout" => "sessions#destroy", as: :signout
   get '/demo', as: :demo, controller: 'welcome', action: 'demo'
   root "welcome#new"
